1cbad3885844da78b128042ba91b38a9227e19a2
Bjarne Svensson <b.svensson@ostfalia.de>
2020-04-20 10:10:32 +0200

    Added .gitignore and added a database setup.

--- navigate.sh
+++ navigate.sh
@@ -1,4 +1,10 @@
-#! /bin/bash
+#! /usr/bin/env bash
+
+database="navigate.sqlite"
+
+function setupDatabase() {
+    sqlite3 $database "CREATE TABLE paths (name varchar, path varchar);"
+}
 
 function add() {
     echo Add
@@ -13,8 +19,15 @@ function list() {
 }
 
 function navigate() {
+    #query=$(sqlite3 navigate.sqlite "SELECT path from paths where name='$1';")
     echo $1
 }
+
+#Setup database if it doesn't exist
+if [[ ! -e ${database} ]]; then
+    echo Database file doesnt exist
+    setupDatabase
+fi
 
 case $1 in 
 "add")
