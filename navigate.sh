bdb976bebab8691c4a8d001f9d9dd280bfb3f51f
Bjarne Svensson <b.svensson@ostfalia.de>
2021-03-13 17:40:46 +0100

    Added a check if a path exists to navigate function

--- navigate.sh
+++ navigate.sh
@@ -2,12 +2,14 @@
 
 database="navigate.sqlite"
 
+# Setup
 function setupDatabase() {
     echo No database found, creating new database..
     sqlite3 $database "CREATE TABLE paths (name varchar, path varchar);"
     echo Done
 }
 
+# Functions for parameters
 function add() {
     sqlite3 $database "INSERT INTO paths VALUES ('$1', '$2');"
 }
@@ -32,8 +34,27 @@ function list() {
 }
 
 function navigate() {
+
+    #Check if paths exist in database
+    CheckForPathInDatabase $1
+    if [ $? -eq 1 ]
+    then 
+        #If so, navigate to it
+        navigate $1
+    else
+        echo Path \"$1\" doesn\'t exists!
+    fi 
+
     query=$(sqlite3 navigate.sqlite "SELECT path from paths where name='$1';")
     cd "$query"
+}
+
+# Helper functiosn
+function CheckForPathInDatabase() {
+    subquery="SELECT name FROM paths WHERE name='$1'"
+    query=$(sqlite3 $database "SELECT EXISTS($subquery) from paths";)
+
+    return $query
 }
 
 #Setup database if it doesn't exist
